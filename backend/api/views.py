24d696c65d437ede412240e38eb1fc19c9017578
venu <venukumarbv@gmail.com>
2025-08-06 12:30:33 +0530

    models to dict from forms

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 import json
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
 from products.models import Product
@@ -9,8 +10,11 @@ def api_home(request, *args, **kwargs):
     print(model_data)
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data["price"] = model_data.price
+        # data['id'] = model_data.id # Id is automatically created by the Models
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data["price"] = model_data.price
+
+        data = model_to_dict(model_data, fields=['title', 'price'])
 
     return JsonResponse(data)
