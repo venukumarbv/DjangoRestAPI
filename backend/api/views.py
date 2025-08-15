18d7a5d52fd2d0cd393314e98e3b7976274ab82f
venu <venukumarbv@gmail.com>
2025-08-15 12:25:55 +0530

    Model Serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,19 +2,28 @@ import json
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 
+from products.serializers import ProductSerializer
 
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    print(model_data)
+    # model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first() # Model serializer way
+
+    print(instance)
     data = {}
-    if model_data:
+    if instance:
         # data['id'] = model_data.id # Id is automatically created by the Models
         # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data["price"] = model_data.price
+        # data = model_to_dict(model_data, fields=['title', 'price'])
 
-        data = model_to_dict(model_data, fields=['title', 'price'])
-
-    return JsonResponse(data)
+        data = ProductSerializer(instance).data  # Model serializer way .data is important
+    return Response(data)   # DRF way
+    # return JsonResponse(data)
---
+++ backend/products/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Product
+        fields = ['title',
+                  'content',
+                  'price',
+                  'sale_price',
+                  "my_discount",
+
+                  ]  # '__all__'
+
+
+    # Method name should be 'get_<variable_name_defined_above>' ; get_my_discount
+    def get_my_discount(self, obj): # obj is important
+        return obj.get_discount() # Calling the method get_discount written in the models.py
