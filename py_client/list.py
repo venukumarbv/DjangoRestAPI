0066265128c07a3ecd99c84596b35856f0614dd4
venu <venukumarbv@gmail.com>
2025-08-16 11:09:11 +0530

    Overrriden TokenAuthentication to have Bearer keyword

--- py_client/list.py
+++ py_client/list.py
@@ -1,8 +1,29 @@
 import requests
+from getpass import getpass
 
+endpoint = "http://localhost:8000/api/auth/"
+username = input("Enter the Username:")
+password = getpass()
 
-endpoint = "http://localhost:8000/api/products/"
+auth_response = requests.post(endpoint, json={
+    "username": username,
+    "password": password
+})
 
-get_response = requests.get(endpoint)
+print(auth_response.json())
 
-print(get_response.json())
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    endpoint = "http://localhost:8000/api/products/"
+    # headers = {
+    #     "Authorization": f"Token {token}"
+    # }
+
+    # Keyword is overridden to have ''Bearer
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    get_response = requests.get(endpoint, headers=headers)
+
+    print(get_response.json())
